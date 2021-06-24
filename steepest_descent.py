d1d820d930eefe7e6799d4f9b071b98600b4f44e
Anaximandro Souza <anaxec1@gmail.com>
2021-06-24 10:20:38 -0300

    Update 02

--- steepest_descent.py
+++ steepest_descent.py
@@ -1,6 +1,26 @@
 import numpy as np
 from scipy.io import mmread
 
-matrix = mmread('/home/anaxsouza/Documents/GitHub/coc757_Trabalho_05/bcsstk19.mtx')
+A = mmread('/home/anaxsouza/Documents/GitHub/coc757_Trabalho_05/bcsstk19.mtx')
 
-print(matrix.todense())
+i = 5000
+
+b = np.zeros(817)
+x = np.ones(817)
+
+r = b - np.dot(A,x)
+r = r[np.newaxis]
+
+delta = np.dot(r.T, r)
+
+print(delta)
+
+
+#delta = np.transpose(r).dot(r)
+
+
+
+'''if np.array_equal(A, z):
+    print('True')
+else:
+    print('False')'''
