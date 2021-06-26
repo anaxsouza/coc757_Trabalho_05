7a77bd3fed6fd1d07a8f77cb9a1869d98a718dda
Anaximandro Souza <anaxec1@gmail.com>
2021-06-26 10:22:34 -0300

    Update 08

--- conjugate_gradient.py
+++ conjugate_gradient.py
@@ -1,32 +1,46 @@
 import numpy as np
 from scipy.io import mmread
 
-A = mmread('/home/anaxsouza/Documents/GitHub/coc757_Trabalho_05/bcsstk15.mtx')
 
-i = 0
-i_max = 5000
-epsilon = .1
+def is_pos_def(x):
+    """check if a matrix is symmetric positive definite"""
+    return np.all(np.linalg.eigvals(x) > 0)
 
+def check_symmetric(x, tol=1e-8):
+    return np.all(np.abs(x-x.T) < tol)
+
+
+A = mmread('/home/anaxsouza/Documents/GitHub/coc757_Trabalho_05/bcsstk15.mtx').todense()
 b = np.zeros((A.shape[0], 1))
 x = np.ones((A.shape[0], 1))
 
-r = b - A@x
-d = r
+i_max = 5000
+epsilon = np.finfo(float).eps
 
-delta_new = r.T@r
+i = 0
+r = b - A @ x
+d = r
+delta_new = r.T @ r
 delta_0 = delta_new
-delta_old = 0
-while i < i_max and delta_new > (epsilon**2) * delta_0:
-    q = A@d
-    alpha = delta_new/(d.T@q)
-    x = x + alpha*d
-    if i % 63 == 0:
-        r = b - A@x
+delta_old = 0.0
+
+#if (is_pos_def(A) == False) | (check_symmetric(A) == False):
+    #raise ValueError('Matrix A needs to be symmetric positive definite (SPD)')
+while (i < i_max) and (delta_new > (epsilon ** 2)*delta_0):
+    print(i)
+    q = A @ d
+    alpha = delta_new/(d.T @ q)
+    alpha = alpha.item()
+    x = x + alpha * d
+    if i % 50:
+        r = b - A @ x
     else:
-        r = r - alpha*q
+        r = r - alpha * q
     delta_old = delta_new
-    delta_new = r.T@r
+    delta_new = r.T @ r
     beta = delta_new/delta_old
-    d = r + beta*d
+    beta = beta.item()
+    d = r + beta * d
     i += 1
-    print(r)
+
+print(x)
--- steepest_descent.py
+++ steepest_descent.py
@@ -1,29 +1,39 @@
 import numpy as np
 from scipy.io import mmread
 
-A = mmread('/home/anaxsouza/Documents/GitHub/coc757_Trabalho_05/bcsstk15.mtx')
+def is_pos_def(x):
+    """check if a matrix is symmetric positive definite"""
+    return np.all(np.linalg.eigvals(x) > 0)
+
+def check_symmetric(x, tol=1e-8):
+    return np.all(np.abs(x-x.T) < tol)
 
 
-i = 0
-i_max = 5000
-epsilon = .1
-
+A = mmread('/home/anaxsouza/Documents/GitHub/coc757_Trabalho_05/bcsstk15.mtx').todense()
 b = np.zeros((A.shape[0], 1))
 x = np.ones((A.shape[0], 1))
 
-r = A@x - b
+i_max = 5000
+epsilon = np.finfo(float).eps
 
-delta = r.T@r
+i = 0
+r = b - A @ x
+delta = r.T @ r
 delta_0 = delta
 
-while i < i_max and delta > (epsilon ** 2)*delta_0:
-    q = A@r
-    alpha = delta/(r.T@q)
-    x = x + r*alpha
-    if i % 63 == 0:
-        r = A@x - b
+#if (is_pos_def(A) == False) | (check_symmetric(A) == False):
+    #raise ValueError('Matrix A needs to be symmetric positive definite (SPD)')
+while (i < i_max) and (delta > (epsilon ** 2)*delta_0):
+    print(i)
+    q = A @ r
+    alpha = delta/(r.T @ q)
+    alpha = alpha.item()
+    x = x + alpha * r
+    if i % 50:
+        r = b - A @ x
     else:
-        r = r - q*alpha
-    delta = r.T@r
+        r = r - alpha * q
+    delta = r.T @ r
     i += 1
-    print(r)
+
+print(x)
